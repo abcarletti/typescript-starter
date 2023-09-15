987be5b5cbb0d198a75ccf3c658f0bd05a49944c
Angel Bolado <bolado.angel@gmail.com>
2023-09-15 19:34:49 +0200

    Revert "Init project and endpoints to manage acoount to cognito"
    This reverts commit 570c33544b4354360185d33e0275113ee48ae2b0.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.development.env',
-      isGlobal: true,
-    }),
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
